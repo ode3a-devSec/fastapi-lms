5227295b1f9f8e0ab521226fc7dff2bca94acad4
ode3a-devSec <techch3f@gmail.com>
2025-08-08 14:12:26 -0400

    first commit

--- lesson03_query_parameters/main.py
+++ lesson03_query_parameters/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# http://127.0.0.1:8000/items/20?q=3
+# http://127.0.0.1:8000/items/20
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: str | None = None):
     if q:
