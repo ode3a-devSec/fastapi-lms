f32d05b741d4031153e29b9a075510aaa9b593c5
ode3a-devSec <techch3f@gmail.com>
2025-08-08 13:43:13 -0400

    first commit

--- lesson02_path_parameters/main.py
+++ lesson02_path_parameters/main.py
@@ -1,11 +1,23 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
 
-# path parameters
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-@app.get("/items/{item_id}")
-async def read_time(item_id):
-    return {"item_id": item_id}
+    return {"model_name": model_name, "message": "Have some residuals"}
