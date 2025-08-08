e27087584e1627089d1e192459150c8aa9b6f69c
ode3a-devSec <techch3f@gmail.com>
2025-08-08 16:35:16 -0400

    first commit

--- lesson03_query_parameters/main.py
+++ lesson03_query_parameters/main.py
@@ -2,21 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-# http://127.0.0.1:8000/users/1/items/2
-# http://127.0.0.1:8000/users/1/items/2?q=hello
-# http://127.0.0.1:8000/users/1/items/2?short=True
+# http://127.0.0.1:8000/items/foo-item?needy=sooooneedy
 
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
     return item
